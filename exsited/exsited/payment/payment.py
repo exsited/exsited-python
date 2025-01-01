9cf1b075ff706de11cd77424b509f6bab960f069
shuvo <shuvo.a@webalive.com.au>
2025-01-01 19:31:14 +1100

    payment module update 01/01/2025

--- exsited/exsited/payment/payment.py
+++ exsited/exsited/payment/payment.py
@@ -1,5 +1,7 @@
+from exsited.exsited.common.common_enum import SortDirection
 from exsited.exsited.payment.dto.payment_dto import PaymentDetailsDTO, PaymentCreateDTO, CardPaymentCreateDTO, \
-    CardDirectDebitPaymentCreateDTO, PaymentInvoiceResponseDTO
+    CardDirectDebitPaymentCreateDTO, PaymentInvoiceResponseDTO, PaymentListDTO, PaymentAccountResponseDTO, \
+    PaymentOrderResponseDTO, PaymentMultipleRequestDTO
 from exsited.exsited.payment.payment_api_url import PaymentApiUrl
 from exsited.common.sdk_util import SDKUtil
 from exsited.http.ab_rest_processor import ABRestProcessor
@@ -10,15 +12,27 @@ class Payment(ABRestProcessor):
         response = self.post(url=PaymentApiUrl.PAYMENT_CREATE.format(invoice_id=invoice_id), request_obj=request_data, response_obj=PaymentDetailsDTO())
         return response
 
-    def create_card(self, invoice_id: str, request_data: CardPaymentCreateDTO) -> PaymentDetailsDTO:
-        response = self.post(url=PaymentApiUrl.PAYMENT_CREATE_CARD.format(invoice_id=invoice_id), request_obj=request_data, response_obj=PaymentDetailsDTO())
-        return response
-
-    def create_direct_debit(self, invoice_id: str, request_data: CardDirectDebitPaymentCreateDTO) -> PaymentDetailsDTO:
-        response = self.post(url=PaymentApiUrl.PAYMENT_CREATE_DIRECT_DEBIT.format(invoice_id=invoice_id),
-                             request_obj=request_data, response_obj=PaymentDetailsDTO())
-        return response
-
-    def details(self, id: str) -> PaymentInvoiceResponseDTO:
+    def invoice_details(self, id: str) -> PaymentInvoiceResponseDTO:
         response = self.get(url=PaymentApiUrl.PAYMENT_DETAILS_INVOICE.format(id=id), response_obj=PaymentInvoiceResponseDTO)
+        return response
+
+    def list(self, limit: int = None, offset: int = None, direction: SortDirection = None, order_by: str = None) -> PaymentListDTO:
+        params = SDKUtil.init_pagination_params(limit=limit, offset=offset, direction=direction, order_by=order_by)
+        response = self.get(url=PaymentApiUrl.PAYMENT_LIST, params=params, response_obj=PaymentListDTO())
+        return response
+
+    def details(self, id: str) -> PaymentDetailsDTO:
+        response = self.get(url=PaymentApiUrl.PAYMENT_DETAILS.format(id=id), response_obj=PaymentDetailsDTO())
+        return response
+
+    def account_details(self, id: str) -> PaymentAccountResponseDTO:
+        response = self.get(url=PaymentApiUrl.PAYMENT_ACCOUNT_DETAILS.format(id=id), response_obj=PaymentAccountResponseDTO())
+        return response
+
+    def order_details(self, id: str) -> PaymentOrderResponseDTO:
+        response = self.get(url=PaymentApiUrl.PAYMENT_ORDER_DETAILS.format(id=id), response_obj=PaymentOrderResponseDTO())
+        return response
+
+    def delete(self, id: str):
+        response = self.delete_request(url=PaymentApiUrl.PAYMENT_DELETE.format(id=id))
         return response
--- tests/payment_test.py
+++ tests/payment_test.py
@@ -3,7 +3,9 @@ from exsited.common.ab_exception import ABException
 from exsited.common.sdk_conf import SDKConfig
 from tests.common.common_data import CommonData
 from exsited.exsited.payment.dto.payment_dto import PaymentCreateDTO, PaymentDataDTO, PaymentAppliedDTO, \
-    CardDirectDebitPaymentAppliedDTO, CardDirectDebitPaymentDataDTO, CardDirectDebitPaymentCreateDTO
+    CardDirectDebitPaymentAppliedDTO, CardDirectDebitPaymentDataDTO, CardDirectDebitPaymentCreateDTO, \
+    PaymentMultipleRequestDTO, PaymentMultipleDataDTO, InvoiceMultipleDTO, PaymentAppliedMultipleDTO, \
+    AdditionalFieldsDTO, CreditAppliedDTO, GiftCertificateDTO, CustomAttributeDTO
 from exsited.exsited.payment.dto.payment_dto import CardPaymentCreateDTO, CardPaymentDataDTO, CardPaymentAppliedDTO
 
 
@@ -14,80 +16,111 @@ def test_payment_details_against_invoice():
     exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
 
     try:
-        response = exsited_sdk.payment.details(id='INV-0YH0A1-0010')
+        response = exsited_sdk.payment.invoice_details(id='')
         print(response)
         return response
-        # ResponseToObj().process(response=response["accounts"][0])
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+def test_payment_list_basic():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.payment.list()
+        print(response)
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+def test_payment_details():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.payment.details(id='')
+        print(response)
+        return response
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+
+def test_payment_account_details():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.payment.account_details(id='')
+        print(response)
+        return response
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+
+def test_payment_order_details():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.payment.order_details(id='')
+        print(response)
+        return response
+
     except ABException as ab:
         print(ab)
         print(ab.get_errors())
         print(ab.raw_response)
 
 def test_payment_create_basic():
-    SDKConfig.PRINT_REQUEST_DATA = True
+    SDKConfig.PRINT_REQUEST_DATA = False
     SDKConfig.PRINT_RAW_RESPONSE = False
+
     exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
     try:
-        payment_applied = PaymentAppliedDTO(processor="", amount="", reference="")
+        payment_applied = PaymentAppliedDTO(processor="Cash", amount="20.00", reference="")
         payment_data = PaymentDataDTO(date="", paymentApplied=[payment_applied], note="")
         request_data = PaymentCreateDTO(payment=payment_data)
+
         response = exsited_sdk.payment.create(invoice_id="", request_data=request_data)
         print(response)
+
     except ABException as ab:
         print(ab)
         print(ab.get_errors())
         print(ab.raw_response)
 
-def test_payment_create_card():
-    SDKConfig.PRINT_REQUEST_DATA = True
+
+def test_payment_delete():
+    SDKConfig.PRINT_REQUEST_DATA = False
     SDKConfig.PRINT_RAW_RESPONSE = False
+
     exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
     try:
-        card_payment_applied = CardPaymentAppliedDTO(
-            processor="",
-            amount="",
-            cardType="",
-            token="",
-            cardNumber="",
-            expiryMonth="",
-            expiryYear="",
-            additionalFields={"host_ip": ""}
-        )
-        card_payment_data = CardPaymentDataDTO(date="", paymentApplied=[card_payment_applied], note="")
-        request_data = CardPaymentCreateDTO(payment=card_payment_data)
-        response = exsited_sdk.payment.create_card(invoice_id="", request_data=request_data)
+        response = exsited_sdk.payment.delete(id="")
         print(response)
+
     except ABException as ab:
         print(ab)
         print(ab.get_errors())
         print(ab.raw_response)
-
-def test_payment_create_direct_debit():
-    SDKConfig.PRINT_REQUEST_DATA = True
-    SDKConfig.PRINT_RAW_RESPONSE = False
-    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
-    try:
-        card_direct_debit_applied = CardDirectDebitPaymentAppliedDTO(
-            processor="",
-            amount="",
-            reference="",
-        )
-        card_direct_debit_data = CardDirectDebitPaymentDataDTO(
-            date="",
-            paymentApplied=[card_direct_debit_applied]
-        )
-        request_data = CardDirectDebitPaymentCreateDTO(payment=card_direct_debit_data)
-        response = exsited_sdk.payment.create_direct_debit(invoice_id="",
-                                                           request_data=request_data)
-        print(response)
-    except ABException as ab:
-        print(ab)
-        print(ab.get_errors())
-        print(ab.raw_response)
-
-
-# Call the test function
-#test_payment_create_card()
-test_payment_create_basic()
-#test_payment_create_direct_debit()
-# test_payment_details_against_invoice()
