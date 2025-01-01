f926a272bfed0a2f32c8e10288e250ec32f1e105
shuvo <shuvo.a@webalive.com.au>
2025-01-01 17:51:15 +1100

    credit note module update 01/01/2025

---
+++ tests/credit_note_test.py
@@ -0,0 +1,71 @@
+from exsited.exsited.exsited_sdk import ExsitedSDK
+from exsited.common.ab_exception import ABException
+from exsited.common.sdk_conf import SDKConfig
+from tests.common.common_data import CommonData
+
+
+def test_credit_note_details():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.credit_note.details(id='')
+        print(response)
+        return response
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+
+def test_credit_note_application_list():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.credit_note.credit_note_application_list()
+        print(response)
+        return response
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+
+def test_credit_note_application_details():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.credit_note.credit_note_application_details(uuid='')
+        print(response)
+        return response
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+def test_invoice_credit_note_application_list():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.credit_note.invoice_credit_note_application_list(id='')
+        print(response)
+        return response
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
