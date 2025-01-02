1cf5f861f07c1fddd20f487ce64191f21abed18d
shuvo <shuvo.a@webalive.com.au>
2025-01-02 13:47:09 +1100

    refund module update 02/01/2025

--- exsited/exsited/exsited_sdk.py
+++ exsited/exsited/exsited_sdk.py
@@ -7,8 +7,8 @@ from exsited.exsited.item.item import Item
 from exsited.exsited.item_fulfillment.item_fulfillment import ItemFulfillment
 from exsited.exsited.order.order import Order
 from exsited.exsited.purchase_order.purchase_order import PurchaseOrder
+from exsited.exsited.refund.refund import Refund
 from exsited.exsited.setting.setting import Setting
-
 from exsited.exsited.payment.payment import Payment
 
 
@@ -20,6 +20,7 @@ class ExsitedSDK:
     setting: Setting = None
     payment: Payment = None
     express: Express = None
+    refund: Refund = None
     purchase_order: PurchaseOrder = None
     item: Item = None
     item_fulfillment: ItemFulfillment = None
@@ -43,6 +44,7 @@ class ExsitedSDK:
         self.setting = Setting(request_token_dto=self._request_token_dto)
         self.payment = Payment(request_token_dto=self._request_token_dto)
         self.express = Express(request_token_dto=self._request_token_dto)
+        self.refund = Refund(request_token_dto=self._request_token_dto)
         self.purchase_order = PurchaseOrder(request_token_dto=self._request_token_dto)
         self.item = Item(request_token_dto=self._request_token_dto)
         self.item_fulfillment = ItemFulfillment(request_token_dto=self._request_token_dto)
---
+++ tests/refund_test.py
@@ -0,0 +1,76 @@
+from exsited.common.ab_exception import ABException
+from exsited.common.sdk_conf import SDKConfig
+from exsited.exsited.exsited_sdk import ExsitedSDK
+from exsited.exsited.refund.dto.refund_dto import RefundDetailsDTO, RefundDataDTO
+from tests.common.common_data import CommonData
+
+
+def test_refund_details():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.refund.details(id="")
+        print(response)
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+
+def test_account_refund_list():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.refund.account_refund_list(id="")
+        print(response)
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+
+def test_refund_create():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        request_data = RefundDetailsDTO(refund=RefundDataDTO(
+            date="",
+            amount="",
+            reference="",
+            note="",
+            paymentMethod="",
+            paymentProcessor=""))
+        response = exsited_sdk.refund.create(cn_id='', request_data=request_data)
+        print(response)
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+
+def test_refund_delete():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.refund.delete(id="")
+        print(response)
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
