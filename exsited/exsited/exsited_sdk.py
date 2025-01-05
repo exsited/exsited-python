6c1783ce3c8b4e78f2e74645c234509bb059cc11
shuvo <shuvo.a@webalive.com.au>
2025-01-05 22:52:27 +1100

    Gift Certificate Module Update 05/01/2025

--- exsited/exsited/exsited_sdk.py
+++ exsited/exsited/exsited_sdk.py
@@ -2,11 +2,7 @@ from exsited.exsited.account.account import Account
 from exsited.exsited.auth.dto.token_dto import RequestTokenDTO
 from exsited.exsited.credit_note.credit_note import CreditNote
 from exsited.exsited.gift_certificates.gift_certificates import GiftCertificates
-# from exsited.exsited.express.express import Express
 from exsited.exsited.invoice.invoice import Invoice
-# from exsited.exsited.item.item import Item
-# from exsited.exsited.item_receipt.item_receipt import ItemReceipt
-# from exsited.exsited.item_fulfillment.item_fulfillment import ItemFulfillment
 from exsited.exsited.order.order import Order
 from exsited.exsited.purchase_order.purchase_order import PurchaseOrder
 from exsited.exsited.refund.refund import Refund
@@ -21,12 +17,8 @@ class ExsitedSDK:
     invoice: Invoice = None
     setting: Setting = None
     payment: Payment = None
-    # express: Express = None
     refund: Refund = None
     purchase_order: PurchaseOrder = None
-    # item_receipt: ItemReceipt = None
-    # item: Item = None
-    # item_fulfillment: ItemFulfillment = None
     gift_certificates: GiftCertificates = None
     credit_note: CreditNote = None
 
@@ -48,12 +40,8 @@ class ExsitedSDK:
         self.invoice = Invoice(request_token_dto=self._request_token_dto)
         self.setting = Setting(request_token_dto=self._request_token_dto)
         self.payment = Payment(request_token_dto=self._request_token_dto)
-        # self.express = Express(request_token_dto=self._request_token_dto)
         self.refund = Refund(request_token_dto=self._request_token_dto)
         self.purchase_order = PurchaseOrder(request_token_dto=self._request_token_dto)
-        # self.item_receipt = ItemReceipt(request_token_dto=self._request_token_dto)
-        # self.item = Item(request_token_dto=self._request_token_dto)
-        # self.item_fulfillment = ItemFulfillment(request_token_dto=self._request_token_dto)
         self.gift_certificates = GiftCertificates(request_token_dto=self._request_token_dto)
         self.credit_note = CreditNote(request_token_dto=self._request_token_dto)
 
