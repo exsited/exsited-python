4309b5dc0d5d0e6e3fb545e27a89c34a54c3dfbc
shuvo <shuvo.a@webalive.com.au>
2025-01-08 22:56:17 +1100

    Setting Module Update 08/01/2025

--- tests/setting_test.py
+++ tests/setting_test.py
@@ -1,6 +1,7 @@
 from exsited.exsited.exsited_sdk import ExsitedSDK
 from exsited.common.ab_exception import ABException
 from exsited.common.sdk_conf import SDKConfig
+from exsited.exsited.setting.dto.setting_dto import VariationCreateRequestDTO, VariationCreateDataDTO, OptionDTO
 from tests.common.common_data import CommonData
 
 def test_get_settings_payment_processor():
@@ -110,6 +111,60 @@ def test_get_settings_shipping_profiles():
 
     try:
         response = exsited_sdk.setting.get_settings_shipping_profiles()
+        print(response)
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+def test_get_settings_components():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.setting.get_settings_components()
+        print(response)
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+def test_get_settings_variations():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        response = exsited_sdk.setting.get_settings_variations()
+        print(response)
+
+    except ABException as ab:
+        print(ab)
+        print(ab.get_errors())
+        print(ab.raw_response)
+
+def test_settings_variations_update():
+    SDKConfig.PRINT_REQUEST_DATA = False
+    SDKConfig.PRINT_RAW_RESPONSE = False
+
+    exsited_sdk: ExsitedSDK = ExsitedSDK().init_sdk(request_token_dto=CommonData.get_request_token_dto())
+
+    try:
+        request_data = VariationCreateRequestDTO(
+            variations=VariationCreateDataDTO(
+                name="dimention 34",
+                displayName="dimention 34",
+                description="Bryan 4Adams - (Everything I Do) UPDATE2",
+                options=[OptionDTO(name="NobabU4PDATEsdk", order=0)]
+            ))
+
+        response = exsited_sdk.setting.variation_update(uuid="80ce7d37-beb5-4eff-af33-f98323a70a5e",
+                                                        request_data=request_data)
         print(response)
 
     except ABException as ab:
